c89d212ba2cbdfa44bcc315fad41bdb03e6abd04
Magmyte <charleshaiwen@gmail.com>
2025-11-13 07:38:22 -0800

    2025-11-13 Update
    - Added flipped tiles for player character
    - Added initialization for key inputs, player object, and other important variables
    - Implemented simple left-right movement mechanic

--- src/main.js
+++ src/main.js
@@ -2,12 +2,22 @@ import { Start } from './scenes/Start.js';
 
 const config = {
     type: Phaser.AUTO,
-    title: 'CMPM 120 Project Skeleton',
+    title: 'CMPM 120 Project 3',
     description: '',
     parent: 'game-container',
     width: 1280,
     height: 720,
     backgroundColor: '#000000',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {
+                x: 0,
+                y: 50
+            },
+            debug: true
+        }
+    },
     pixelArt: false,
     scene: [
         Start
--- src/scenes/Start.js
+++ src/scenes/Start.js
@@ -5,19 +5,94 @@ export class Start extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('background', 'assets/bg.png');
 
+        // character image assets
+        this.load.image('characterSmallLeft1', 'assets/kenney_pixel-platformer/Tiles/Characters/tile_0018.png');
+        this.load.image('characterSmallLeft2', 'assets/kenney_pixel-platformer/Tiles/Characters/tile_0019.png');
+        this.load.image('characterSmallRight1', 'assets/kenney_pixel-platformer/Tiles/Characters/tile_0027.png');
+        this.load.image('characterSmallRight2', 'assets/kenney_pixel-platformer/Tiles/Characters/tile_0028.png');
+
+        this.load.image('characterSmallLeftDown', 'assets/kenney_pixel-platformer/Tiles/Characters/tile_0020.png');
+        this.load.image('characterSmallRightDown', 'assets/kenney_pixel-platformer/Tiles/Characters/tile_0029.png');
+
+        this.load.image('characterBigLeft1', 'assets/kenney_pixel-platformer/Tiles/Characters/tile_0021.png');
+        this.load.image('characterBigLeft2', 'assets/kenney_pixel-platformer/Tiles/Characters/tile_0022.png');
+        this.load.image('characterBigRight1', 'assets/kenney_pixel-platformer/Tiles/Characters/tile_0030.png');
+        this.load.image('characterBigRight2', 'assets/kenney_pixel-platformer/Tiles/Characters/tile_0031.png');
+
+        this.load.image('characterBigLeftDown', 'assets/kenney_pixel-platformer/Tiles/Characters/tile_0023.png');
+        this.load.image('characterBigRightDown', 'assets/kenney_pixel-platformer/Tiles/Characters/tile_0032.png');
+
+        // coin image asset
+        this.load.image('coin', 'assets/kenney_pixel-platformer/Tiles/tile_0180.png');
     }
 
     create() {
-        this.background = this.add.sprite(640, 320, 'background');
-        this.last_time = 0;
+        
+        // grab size of screen
+        const {width, height} = this.scale;
+
+        // initialize player object
+        this.player = this.physics.add.sprite(width / 2, height / 2, 'characterSmallRight1').setScale(3);
+
+        // player variables
+        this.player.small = true; // boolean for declaring character size - set to false if character is big
+
+        this.player.accelerationXS = 100;
+
+        this.player.accelerationXB = 60;
+
+        this.player.velocityMaxXS = 200;
+        this.player.velocityMaxYS = 200;
+
+        this.player.velocityMaxXB = 125;
+        this.player.velocityMaxYB = 300;
+
+        // declare maximum velocity in x and y directions
+        this.player.setMaxVelocity(this.player.velocityMaxXS, this.player.velocityMaxYS);
+
+        this.player.velocityX = 0;
+        this.player.velocityY = 0;
+
+        // grab key inputs
+        this.keyW = this.input.keyboard.addKey("W", false, true);
+        this.keyA = this.input.keyboard.addKey("A", false, true);
+        this.keyS = this.input.keyboard.addKey("S", false, true);
+        this.keyD = this.input.keyboard.addKey("D", false, true);
+
+        this.up = this.input.keyboard.addKey("UP", false, true);
+        this.left = this.input.keyboard.addKey("LEFT", false, true);
+        this.down = this.input.keyboard.addKey("DOWN", false, true);
+        this.right = this.input.keyboard.addKey("RIGHT", false, true);
+
+        this.space = this.input.keyboard.addKey("SPACE", false, true);
     }
 
-    update(time) {
-        let dt = (time - this.last_time)/1000;
-        this.last_time = time;
+    update(time, dTime) {
         
+        // check movement
+        let moveX = 0;
+
+        if (this.keyA.isDown || this.left.isDown)
+        {
+            moveX--;
+        }
+        if (this.keyD.isDown || this.right.isDown)
+        {
+            moveX++;
+        }
+
+        if (moveX != 0)
+        {
+            if (this.player.small)
+            {
+                this.player.velocityX += moveX * this.player.accelerationXS * dTime / 1000;
+            }
+            else
+            {
+                this.player.velocityX += moveX * this.player.accelerationXB * dTime / 1000;
+            }
+        }
     }
     
 }
